1d772d7bab6a8230cf5c29cd8f4933b06cb8a8c6
soumic sarkar <soumic@codillion.com>
2025-06-16 20:20:32 +0530

    fix: update BACKEND_URL to use HTTPS for improved security

--- src/config.tsx
+++ src/config.tsx
@@ -1,5 +1,5 @@
 export const BACKEND_URL2 =
   "https://150a-2405-201-8002-78df-c165-d1b3-2d00-e47a.ngrok-free.app";
 // export const BACKEND_URL = "https://quantum-backend-hw57.onrender.com";
-export const BACKEND_URL = "http://quantum-turf-api-env.eba-8uvgbytc.us-east-1.elasticbeanstalk.com";
+export const BACKEND_URL = "https://quantum-turf-api-env.eba-8uvgbytc.us-east-1.elasticbeanstalk.com";
 export const CLOUDFRONT_URL = "https://d2bvviipd4yuuo.cloudfront.net";
